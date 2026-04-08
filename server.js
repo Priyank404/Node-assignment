c4655dad647d1d156395771c16385eda1074bb0a
Priyank404 <priyank.khambhati.06@gmail.com>
2026-04-08 23:13:36 +0530

    fix: bind to 0.0.0.0

--- server.js
+++ server.js
@@ -9,5 +9,6 @@ app.use(express.json());
 app.use("/api", schoolRoute);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
