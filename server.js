29d01a318a6c3bbb470a586b5b28740d5889702d
Priyank404 <priyank.khambhati.06@gmail.com>
2026-04-08 21:39:51 +0530

    optimizing code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import connectDB from "./config/db.js";
 import schoolRoute from "./Rotues/schoolRoute.js";
 const app = express();
 
@@ -7,9 +6,6 @@ app.use(express.json());
 
 app.use("/api", schoolRoute);
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
